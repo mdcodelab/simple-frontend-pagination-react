522ef403ea5f8e4e2089739c1b26e976cdfefa42
miha11-hash <d_mihaela@msn.com>
2024-03-16 12:57:12 +0200

    pagination functionality

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
 import Posts from "./components/Posts";
-//from JSON placeholder
 
 function App() {
-  const [posts, setPosts]=React.useState([]);
-  const [loading, setLoading]=React.useState(false);
-  const [currentPage, setCurrentPage]=React.useState(1);
-  const [postsPerPage, setPostsPerPage]=React.useState(10);
+  const [posts, setPosts] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [postsPerPage, setPostsPerPage] = React.useState(10);
 
   React.useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts");
+        "https://jsonplaceholder.typicode.com/posts"
+      );
       setPosts(response.data);
       setLoading(false);
     };
     fetchData();
   }, []);
 
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
-  console.log(posts);
+  const pageNumber = [];
+  for (let i = 1; i <= Math.ceil(posts.length / postsPerPage); i++) {
+    pageNumber.push(i);
+  }
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="app">
-    <h1>My blog items</h1>
-      <Posts posts={posts} loading={loading}></Posts>
+      <h1>My blog items</h1>
+      <Posts posts={currentPosts} loading={loading}></Posts>
+      <div className="pagination">
+        {pageNumber.map((number) => (
+          <a href="#" key={number} onClick={() => paginate(number)}>
+            {number}
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
-
